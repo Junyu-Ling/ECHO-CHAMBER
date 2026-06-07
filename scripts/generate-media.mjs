ceca30d96b7e892a85f35571d78bb91e15b0a4a4
LingJ <LingJunYu20081201@gmail.com>
2026-06-07 10:25:22 +0800

    feat: 更新白千禾成员照片

--- scripts/generate-media.mjs
+++ scripts/generate-media.mjs
@@ -268,7 +268,7 @@ const memberSources = [
   ["shen-xinyu.png", "shen-xinyu.webp", {}],
   ["richard.png", "richard.webp", {}],
   ["ellis.png", "ellis.webp", {}],
-  ["bai-qianhe.png", "bai-qianhe.webp", { position: "top", maxUpscale: 1.65 }],
+  ["bai-qianhe.png", "bai-qianhe.webp", { position: "centre", maxUpscale: 2.2 }],
   ["gu-chenyang.png", "gu-chenyang.webp", {}],
   ["huang-ziyi.png", "huang-ziyi.webp", { position: "centre", maxUpscale: 2.2, cropZoom: 1.08 }],
   ["liu-yiyang.png", "liu-yiyang.webp", { position: "centre", maxUpscale: 2.2 }],
